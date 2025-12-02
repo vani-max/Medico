a552b0fbc8bd03449809bc9ffd12c2b382a1a9af
vani <vani.vashishtha2024@nst.rishihood.edu.in>
2025-12-02 17:29:11 +0530

    fix

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../components/Header'
-import SpecialityMenu from '../components/specialityMenu'
+import SpecialityMenu from '../components/SpecialityMenu'
 import TopDoctors from '../components/TopDoctors'
 import Banner from '../components/Banner'
 
